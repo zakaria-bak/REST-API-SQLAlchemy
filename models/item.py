a81c9e8ad03503bc5aeb9f9b8987f238e654deff
zakaria-bak <zakariabakziz1@gmail.com>
2022-03-02 01:49:26 +0100

    adding the item.py store.py user.py models files

---
+++ models/item.py
@@ -0,0 +1,28 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float())
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def json(self):
+        return {'name' : self.name, 'price' : self.price}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return ItemModel.query.filter_by(name=name).first()
+
+    def save_to_db(self):  # inserting & updating
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+        
